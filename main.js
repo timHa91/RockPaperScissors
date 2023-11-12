1207db939e3077954205da1f8bc720d4335e6772
timHa91 <tim.andre.hartmann@gmail.com>
2023-11-12 17:20:27 +0100

    Created a UI

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-const roundLength = 5
 const choice = ['Rock', 'Paper', 'Scissor'];
-game();
+let playerSelection = '';
+const maxScore = 5;
 
-function game () {
-    let round = 1;
-    while(round <= roundLength) {
-        round++;
-        const playerSelection = prompt('Rock, Paper or Scissor?');
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        playerSelection = e.target.innerText.toLowerCase();
+        playRound(playerSelection, getComputerChoice())
+    })
+})
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choice.length);
@@ -19,20 +17,44 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     const computer = computerSelection.toLowerCase();
-    const player = playerSelection.toLowerCase();
-    const playerSelectionFormated = player[0].toUpperCase() + player.slice(1);
+    const playerSelectionFormated = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+
     const rules = {
         rock: 'scissor',
         scissor: 'paper',
         paper: 'rock'
     }
 
-    if(player == computer) {
-        return 'Draw! Go again';
+    if(playerSelection == computer) {
+        displayRoundResult('Draw! Go again');
     }
-    else if (computer == rules[player]) {
-        return `You Win! ${playerSelectionFormated} beats ${computerSelection}`
+    else if (computer == rules[playerSelection]) {
+        displayRoundResult(`You Win! ${playerSelectionFormated} beats ${computerSelection}`);
+        addScore('player');
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelectionFormated}`;
+        displayRoundResult(`You Lose! ${computerSelection} beats ${playerSelectionFormated}`);
+        addScore('computer');
     }
 }
+
+function displayRoundResult(message){
+    const result = document.querySelector('.round');
+    result.innerText = message;
+}
+
+function addScore(who) {
+    const score = document.querySelector(`.${who} span`);
+    score.innerText = +score.innerText + 1;
+    console.log(score.innerText);
+    if(score.innerText == maxScore) declareWinner(who);
+}
+
+function declareWinner(who) {
+    const playerWon = 'Gratulation. You won';
+    const playerLose = 'You Lost. Try again';
+    const displayResult = document.querySelector('.container');
+    who === 'player' ? displayResult.innerText = playerWon : displayResult.innerText = playerLose;
+}
+
+
+
